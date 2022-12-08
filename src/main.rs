df5ce55dcc0aa8e3808e7578811a9f36fa421815
LoryMaster <lorenzo.stramondo@gmail.com>
2022-12-08 18:31:24 +0100

    Replace the ligature.

--- src/main.rs
+++ src/main.rs
@@ -737,6 +737,7 @@ fn add_entry_if_missing_u32(buf: &mut ByteBuffer, entry_data_str: &str) -> u32
     let replace_shit = replace_shit.replace("\u{2019}", "'");
     let replace_shit = replace_shit.replace("\u{201c}", "\"");
     let replace_shit = replace_shit.replace("\u{201d}", "\"");
+    let replace_shit = replace_shit.replace("\u{fb02}", "fl");
     let entry_data   = replace_shit.as_bytes();
     let entry_len    = replace_shit.len();
     
@@ -779,6 +780,7 @@ fn add_entry_if_missing(buf: &mut ByteBuffer, entry_data_str: &str) -> u16
     let replace_shit = replace_shit.replace("\u{2019}", "'");
     let replace_shit = replace_shit.replace("\u{201c}", "\"");
     let replace_shit = replace_shit.replace("\u{201d}", "\"");
+    let replace_shit = replace_shit.replace("\u{fb02}", "fl");
     let entry_data   = replace_shit.as_bytes();
     let entry_len    = replace_shit.len();
     
